47531a8522211d7cff2e8e39a63f5fb3b45fbf21
cnicolau <cnicolau@travelrepublic.co.uk>
2016-05-25 16:41:54 +0100

    some cleanup

--- get_reviews/Topics.py
+++ get_reviews/Topics.py
@@ -1,18 +1,50 @@
 
 # coding: utf-8
 
-# In[4]:
+# # <p style="text-align: center;"> Title </p>
 
-from pre_process import get_rate_setter_dictionary_corpus
+# In[1]:
+
+# general imports 
+from pre_process import get_all_reviews, get_rate_setter_dictionary_corpus, pretty_print_html 
 from gensim import corpora, models, similarities
 from gensim.models import LdaMulticore
-from gensim.ldamodel import ldamodel 
+from gensim.models.ldamodel import LdaModel
 from pyLDAvis.gensim import prepare
+from random import randint
+import pyLDAvis 
+from IPython.display import HTML
+from pre_process import ReviewsRetriever, ReviewNormalizer
 
 
-# In[6]:
+# #### We get the reviews by scraping TrustPilot's website and we take a look at a couple of them
 
-dictionary, corpus = get_rate_setter_dictionary_corpus()
+# In[5]:
+
+rr = ReviewsRetriever()
+reviews = rr.get(cached=True)
+pretty_print_html([reviews[randint(0, len(reviews))], reviews[randint(0, len(reviews))]])
+
+
+# #### We now normalize the reviews: remove punctuation, make everything lowercase and stem. Then we take a look at another couple of them. 
+
+# In[10]:
+
+rn = ReviewNormalizer()
+normalized_reviews = [rn.tokenize(r)
+                      for r in reviews]
+pretty_print_html([" ".join(normalized_reviews[randint(0, len(normalized_reviews))]), 
+                   " ".join(normalized_reviews[randint(0, len(normalized_reviews))])])
+
+
+# #### Training the model (this might take a while...)
+
+# In[12]:
+
+dictionary = corpora.Dictionary(normalized_reviews)
+corpus = [dictionary.doc2bow(r)
+          for r in normalized_reviews]
+lda = LdaModel(corpus=corpus, num_topics=5, id2word=dictionary, passes=100)
 
 
 # In[16]:
@@ -23,7 +55,7 @@ print(dictionary)
 
 # In[8]:
 
-lda = LdaMulticore(corpus=corpus, num_topics=5, id2word=dictionary, passes=100)
+lda = LdaModel(corpus=corpus, num_topics=5, id2word=dictionary, passes=100)
 
 
 # In[10]:
@@ -36,15 +68,11 @@ lda.print_topics()
 dictionary[lda.get_topic_terms(0)[0][0]]
 
 
-# In[12]:
-
-prepared_data = prepare(lda, corpus, dictionary)
-
+# #### Prepare data and visualize!
 
 # In[14]:
 
-import pyLDAvis 
-# vis_data = pyLDAvis.prepare(**prepared_data)
+prepared_data = prepare(lda, corpus, dictionary)
 pyLDAvis.display(prepared_data)
 
 
--- get_reviews/pre_process.py
+++ get_reviews/pre_process.py
@@ -4,19 +4,27 @@ from bs4.element import NavigableString
 from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer
 import re
-from gensim import corpora, models, similarities
-from gensim.models.ldamodel import LdaModel
-import pyLDAvis
-from pyLDAvis.gensim import prepare
+from IPython.display import HTML
+import joblib as jb
+
 
 class ReviewNormalizer(object):
+    """
+    Turns a sentence into its tokenized version: no punctuation, no stopwords and stemmed.
+    """
     def __init__(self):
         self._stemmer = PorterStemmer()
         self._stop_words = set(stopwords.words('english'))
         self._stop_words.add("ratesetter")
+        self._stop_words.add("setter")
 
-    def tokenize(self, sentence):
-        words = re.sub("[^a-zA-Z]", " ", sentence)\
+    def tokenize(self, phrase):
+        """
+        Tokenizes a sentence
+        :param phrase: string that contains a phrase (sentences separated by punctuation or space).
+        :return: list of stemmed words
+        """
+        words = re.sub("[^a-zA-Z]", " ", phrase)\
             .lower()\
             .split()
         return [self._stemmer.stem(w)
@@ -24,45 +32,48 @@ class ReviewNormalizer(object):
                 if w not in self._stop_words]
 
 
+class ReviewsRetriever(object):
+    def __init__(self):
+        self._cache_file = "reviews.bin"
+
+    def get(self, cached=False):
+        if cached:
+            return jb.load(self._cache_file)
+        else:
+            pages = ["https://uk.trustpilot.com/review/ratesetter.com"] +\
+                    ["https://uk.trustpilot.com/review/ratesetter.com?page={0}".format(x) for x in range(2, 69)]
+
+            pages_content = [get_content_from_url(p)
+                             for p in pages]
+            reviews = [r
+                       for pc in pages_content
+                       for r in self._parse_reviews(pc)]
+            jb.dump(reviews, self._cache_file)
+            return reviews
+
+    def _parse_reviews(self, page_content):
+        # this preserves the order
+        title_elements = page_content.findAll("a", {"class": "review-title-link"})
+        titles = [str(text.string)
+                  for text in title_elements]
+        review_text_elements = page_content.findAll("div", {"class": "review-body"})
+        review_texts = [[str(paragraph)
+                         for paragraph in review.contents
+                         if type(paragraph) is NavigableString]
+                        for review in review_text_elements]
+
+        l = min(len(titles), len(review_texts))
+        return [" ".join([titles[x]] + review_texts[x])
+                for x in range(l)]
+
+
 def get_content_from_url(url):
     return BeautifulSoup(urlr.urlopen(url).read(), "lxml")
 
 
-def get_reviews(page_content):
-    # this preserves the order
-    title_elements = page_content.findAll("a", {"class": "review-title-link"})
-    titles = [str(text.string)
-              for text in title_elements]
-    review_text_elements = page_content.findAll("div", {"class": "review-body"})
-    review_texts = [[str(paragraph)
-                     for paragraph in review.contents
-                     if type(paragraph) is NavigableString]
-                    for review in review_text_elements]
-
-    l = min(len(titles), len(review_texts))
-    return [" ".join([titles[x]] + review_texts[x])
-            for x in range(l)]
-
-
-def get_rate_setter_dictionary_corpus():
-    pages = ["https://uk.trustpilot.com/review/ratesetter.com"] +\
-            ["https://uk.trustpilot.com/review/ratesetter.com?page={0}".format(x) for x in range(2, 69)]
-
-    pages_content = [get_content_from_url(p)
-                     for p in pages]
-    reviews = [r
-               for pc in pages_content
-               for r in get_reviews(pc)]
-    rn = ReviewNormalizer()
-    normalized_reviews = [rn.tokenize(r)
-                          for r in reviews]
-    dictionary = corpora.Dictionary(normalized_reviews)
-    corpus = [dictionary.doc2bow(r)
-              for r in normalized_reviews]
-    return dictionary, corpus
-
-
-
+def pretty_print_html(items):
+    return HTML('<div class="output_subarea output_html rendered_html output_result" style="padding-right: 100px">{0}</div>'
+                    .format("</br></br>".join(items)))
 
 
 
